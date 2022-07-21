f536e80ca1592ef259fdbb152d97e93aadecf01a
o.sow <o.sow@onlineformapro.com>
2022-07-21 11:42:41 +0200

    add swagger

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -92,7 +92,7 @@ public class AuthController {
                 }
             });
         }
-       user.setRoles(roles);
+        user.setRoles(roles);
         userRepository.save(user);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
